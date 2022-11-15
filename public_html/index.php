2f611ab3d2a13126293b13a0199db2b1b0bb36c6
pedro@DESKTOP-KS93E90 <pedri397gmail.com>
2022-11-15 17:25:44 +0100

    Login

--- public_html/index.php
+++ public_html/index.php
@@ -6,12 +6,44 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>JS>PHP</title>
+        <link rel="stylesheet" href="css/login.css">
     </head>
     <body>
-        <?php
-        echo "Hola!";
-        echo "Soy pedro";
-        ?>
+        <div class="caja__login">
+
+            <h2 class="login__titulo">JS>PHP</h2>
+
+            <form action="#" method="post">
+
+                <div class="login__usuario">
+                    <input type="text" name="" required="">
+                    <label>Usuario</label>
+                </div>
+
+                <div class="login__usuario">
+                    <input type="password" name="" required="">
+                    <label>Contraseña</label>
+                </div>
+
+                <a class="enviar" href="#">
+                    <span class="linea"></span>
+                    <span class="linea"></span>
+                    <span class="linea"></span>
+                    <span class="linea"></span>
+                    Entrar
+                </a>
+                
+                <a class="enviar" href="#">
+                    <span class="linea-reg"></span>
+                    <span class="linea-reg"></span>
+                    <span class="linea-reg"></span>
+                    <span class="linea-reg"></span>
+                    Registrar
+                </a>
+            
+            </form>
+        </div>
+
     </body>
 </html>
