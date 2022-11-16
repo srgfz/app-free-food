475caae50ff98a8c89f8a2002a5e5538b485ce7a
srgfz <fernandezsergio10@gmail.com>
2022-11-16 16:40:38 +0100

    Add filter functions filter and login connection BD

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,23 @@
+<?php
+include "../resources/library/funciones.php";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {//Si recibe un método POST
+    //Guardo el id user, la pass y el rol de la BD:
+    try {
+        $bd = BDconexion("mysql:dbname=appcomida;host=127.0.0.1", "root", "");
+        $sql = "SELECT userId, pass,rol FROM usuarios";
+        $users = $bd->query($sql);
+        //Se cierra la conexión
+        $bd = null;
+    } catch (Exception $ex) {
+        echo "Error con la base de datos: " . $ex->getMessage();
+    }
+    //Guardo si el POST de usuario y contraseña
+    $userLogin = filtrarInput("userLogin", "POST");
+    $passLogin = filtrarInput("passLogin", "POST");
+    echo $userLogin." - ".$passLogin;
+}
+?>
+
 <!DOCTYPE html>
 <!--
 Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -14,26 +34,26 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 
             <h2 class="login__titulo">JS>PHP</h2>
 
-            <form action="#" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
                 <div class="login__usuario">
-                    <input type="text" name="" required="">
+                    <input type="text" name="userLogin" required="">
                     <label>Usuario</label>
                 </div>
 
                 <div class="login__usuario">
-                    <input type="password" name="" required="">
+                    <input type="password" name="passLogin" required="">
                     <label>Contraseña</label>
                 </div>
 
-                <a class="enviar" href="#">
+                <button class="enviar" type="submit">
                     <span class="linea"></span>
                     <span class="linea"></span>
                     <span class="linea"></span>
                     <span class="linea"></span>
                     Entrar
-                </a>
-                
+                </button>
+
                 <a class="enviar" href="#">
                     <span class="linea-reg"></span>
                     <span class="linea-reg"></span>
@@ -41,7 +61,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                     <span class="linea-reg"></span>
                     Registrar
                 </a>
-            
+
             </form>
         </div>
 
